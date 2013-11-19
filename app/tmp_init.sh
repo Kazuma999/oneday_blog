11f55314d063f89ad6c36d03e255cc61f5e538ab
Shohei Yamasaki <shohei.yamasaki@slogan.jp>
2013-11-19 17:09:56 +0900

    tmpディレクトリを生成するシェルスクリプトを追加

---
+++ app/tmp_init.sh
@@ -0,0 +1,10 @@
+mkdir tmp
+mkdir tmp/tmp
+mkdir tmp/cache
+mkdir tmp/cache/models
+mkdir tmp/cache/views
+mkdir tmp/cache/persistent
+mkdir tmp/sessions
+mkdir tmp/tests
+mkdir tmp/logs
+chmod -R 777 tmp/
